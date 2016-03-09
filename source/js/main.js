1f079b10e187daab3d068fd5c9d9013ba6981fdb
Thiago de Bastos <thiago@thiagodebastos.com>
2016-03-09 16:36:26 +1100

    add rotation to mobile icon on home mobile page

--- source/js/main.js
+++ source/js/main.js
@@ -3,16 +3,18 @@ const timelineProgress = (position) => {
   console.log(`[custom] animation ${position}`);
 }
 const slideIn = new TimelineMax({
-  paused: true,
-  repeatDelay: 1.5,
+  paused: true
 });
 
 const sliderPop = new TimelineMax({paused:false})
+const phoneTwist = new TimelineMax({paused:false})
 // ANIMATION CODE
 sliderPop.to('.swipe_toggle', 0.5, {x:-10, yoyo: true, repeat:-1});
 slideIn.to('.content, footer', 0.5, {opacity:0}, '-=0.5');
 slideIn.to('.mobile--slide', 0.5, {left:10}), '-=0.5';
 slideIn.to('.swipe_toggle', 0.5, {rotation:'0deg'}, '-=0.5');
+
+phoneTwist.to('.phone', 0.5, {rotation:'0deg', yoyo:true, repeat:-1, repeatDelay: 2});
 
 const toggleSlide = () => {
   sliderPop.pause();
