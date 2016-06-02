ada19e617b84f0a3f75e21f697308e8feb060cc7
Thiago de Bastos <thiago@thiagodebastos.com>
2016-06-02 10:17:04 +1000

    add fullscreen button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
-'use strict';
+"use strict";
+
+// FULLSCREEN API
+document.getElementById("button--fullscreen").addEventListener("click", function () {
+  toggleFullScreen();
+}, false);
 
 // ANIMATION SETTINGS
 var timelineProgress = function timelineProgress(position) {
-  console.log('[custom] animation ' + position);
+  console.log("[custom] animation " + position);
 };
 var slideIn = new TimelineMax({
   paused: true
@@ -33,9 +38,35 @@ swipeToggle.onclick = toggleSlide;
 $(".video").click(function (e) {
   e.preventDefault();
   var videoID = $(this).attr("videoID");
-  $(this).html('<iframe src="//player.vimeo.com/video/' + videoID + '" width="100%" height="100%" title="0" frameborder="0" autoplay="1" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+  $(this).html("<iframe src=\"//player.vimeo.com/video/" + videoID + "\" width=\"100%\" height=\"100%\" title=\"0\" frameborder=\"0\" autoplay=\"1\" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>");
   $(this).css('width: 100vw; height:100vh');
   console.log(this);
 });
 "use strict";
+
+function toggleFullScreen() {
+  if (!document.fullscreenElement && // alternative standard method
+  !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {
+    // current working methods
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen();
+    } else if (document.documentElement.msRequestFullscreen) {
+      document.documentElement.msRequestFullscreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullscreen) {
+      document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+}
 //# sourceMappingURL=main.js.map
